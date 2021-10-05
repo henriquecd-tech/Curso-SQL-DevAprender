699d0441b43d3a08cb2441fcde4d9e4fd8e23515
Henrique Daniel <henriquec.daniel@hotmail.com>
2021-10-05 17:38:22 -0300

    anotações e exercicios de SQL - SELECT e FROM

---
+++ Ex01_SQL.py
@@ -0,0 +1,7 @@
+# a equipe de marketing precisa fazer uma pesquisa sobre os nomes mais comuns de seus clientes e precisa do nome e
+# sobrenome de todos os clientes que estão cadastrados no sistema.
+# retornar o first name, last name
+
+# resolução
+# SELECT FirstName, LastName FROM person.Person;
+# https://paste.pics/3a1adc9efb8475974a3e64412611ae26
---
+++ anotações.py
@@ -0,0 +1,10 @@
+# instalação do SQL + SSMS
+# criação de bancos via interface SSMS e comandos -> create database nome_do_banco; e executar (f5)
+# derrubar / excluir dbs -> drop database nome_do_banco; ou botão direito -> excluir
+# comando mais básico e um dos mais usados no SQL -> SELECT
+# SELECT -> comando universal do SQL (a ideia básica é selecionar uma coluna ou mais na tabela)
+
+# SEELCT nome_da_coluna1, nome_da_coluna2 FROM nome_da_tabela; -> extrai as informações que estão dentro dessas colunas
+
+# SELECT * FROM nome_da_tabela; -> O * indica que todas as colunas serão selecionadas
+# por convenção, todos os comandos em SQL são em letra maiuscula
