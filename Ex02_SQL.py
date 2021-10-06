233518ddb815f8d950960bc090a5b94e0943ab63
Henrique Daniel <henriquec.daniel@hotmail.com>
2021-10-05 21:25:32 -0300

    exercicio básico e anotações sobre DISTINCT

---
+++ Ex02_SQL.py
@@ -0,0 +1,6 @@
+# Quantos sobrenomes únicos nós temos em nossa tabela person.person?
+
+# resolução do exercício
+# SELECT DISTINCT LastName FROM person.Person;
+# http://paste.pics/4c8129cce0a26673e8ba9b7300d5993b
+
--- anotações.py
+++ anotações.py
@@ -2,9 +2,16 @@
 # criação de bancos via interface SSMS e comandos -> create database nome_do_banco; e executar (f5)
 # derrubar / excluir dbs -> drop database nome_do_banco; ou botão direito -> excluir
 # comando mais básico e um dos mais usados no SQL -> SELECT
+
+
+'''SELECT E FROM'''
 # SELECT -> comando universal do SQL (a ideia básica é selecionar uma coluna ou mais na tabela)
-
 # SEELCT nome_da_coluna1, nome_da_coluna2 FROM nome_da_tabela; -> extrai as informações que estão dentro dessas colunas
-
 # SELECT * FROM nome_da_tabela; -> O * indica que todas as colunas serão selecionadas
 # por convenção, todos os comandos em SQL são em letra maiuscula
+
+
+'''DISTINCT'''
+# é usado para omitir dados duplicados de uma tabela e retorna somente dados únicos.
+# sintaxe -> SELECT DISTINCT nome_da_coluna1, nome_da_coluna2 FROM nome_da_tabela
+# exemplo prático -> SELECT DISTINCT FirstName FROM Person.Person;
